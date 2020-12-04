e46dee074ba73a49bfc0c664f82aae1dd5a9c1f0
ethickfox <ethickfox@gmail.com>
2020-12-04 19:21:50 +0300

    During webinar

--- src/com/skillfactory/Main.java
+++ src/com/skillfactory/Main.java
@@ -4,5 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        long l = 9_123_123_123_123_123L;
+        char c1 = '\u2622';
+        char c2 = 'c';
+        char c3 = 42;
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        float f = 123.2f;
+        double d = 123.2;
+
+//        byte b1 = 22;
+//        byte b2 = 129;
+//        byte b3 = (byte)127;
+
+//        System.out.println(b1);
+//        System.out.println(b2);
+//        System.out.println(b3);
+
     }
 }
